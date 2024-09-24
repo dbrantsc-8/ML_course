698b505ecd5ef3b74364d8c73fbd5c8466c654bb
dbrantsc-8 <david.brantschen@epfl.ch>
2024-09-24 17:23:50 +0200

    Part 1 done

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -5,11 +5,19 @@ import numpy as np
 
 
 def build_poly(x, degree):
-    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    """polynomial basis functions for input data x, for j=0 up to j=degree.
+
+    Args:
+        x: numpy array of shape (N,), N is the number of samples.
+        degree: integer.
+
+    Returns:
+        poly: numpy array of shape (N,d+1)"""
+    
     # ***************************************************
-    # INSERT YOUR CODE HERE
-    # polynomial basis function: TODO
-    # this function should return the matrix formed
-    # by applying the polynomial basis to the input data
+    poly = np.ones((len(x), degree + 1))
+    for exp in range(1, degree+1):
+        poly[:, exp] = np.power(x, exp)
+    return poly
     # ***************************************************
     raise NotImplementedError
