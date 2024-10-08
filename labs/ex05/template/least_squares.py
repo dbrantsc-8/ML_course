b0d4059caa4790a28a6cdbcd55a0a6274d3359a5
dbrantsc-8 <david.brantschen@epfl.ch>
2024-10-08 11:15:46 +0200

    started ex05

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -7,11 +7,18 @@ Least Square
 import numpy as np
 
 
+def compute_loss(y, tx, w):
+    loss = 1/(2*tx.shape[0]) * np.sum((y - tx.dot(w))**2) # only one dimension for the sum => no need to specify axis along which we want to sum
+    return loss
+
 def least_squares(y, tx):
     """calculate the least squares."""
     # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
+    gram = tx.T.dot(tx)
+    w = np.linalg.solve(gram, tx.T.dot(y)) # it's good practice to use solve and not inv !!
+    mse = compute_loss(y, tx, w)
+    
+    return w, mse
     # returns mse, and optimal weights
     # ***************************************************
     raise NotImplementedError
